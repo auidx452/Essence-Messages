897a784c02257e0bd6871615951d98ea569b1636
auidx452 <auidx452@gmail.com>
2026-09-13 22:43:57 +0800

    Update 260210-2.js

--- data/260210-2.js
+++ data/260210-2.js
@@ -1,5 +1,5 @@
 /* ============================================================
- * 聊天记录数据文件：2026-02-10 · 存图 2
+ * 聊天记录数据文件：2026-02-10 · 几把
  *
  * 消息格式说明：
  *   { type: "time",   text: "2026年02月10日 00:00" }   时间分隔条
@@ -7,18 +7,23 @@
  *   { type: "msg", name: "昵称", text: "文字内容" }     群友消息（白气泡靠左）
  *   { type: "msg", name: "Auidx", text: "...", self: true }  自己的消息（绿气泡靠右）
  *   { type: "msg", name: "昵称", img: "images/xxx.jpg" }     图片消息
+ *   { type: "msg", name: "昵称", video: "videos/xxx.mp4" }   视频消息（气泡内可播放）
  *   { type: "msg", name: "昵称", img: "...", text: "附言" }  图片+附言
  * ============================================================ */
 
 const AVATARS = {
-  "Auidx": "avatars/Auidx.png",
+  "岛": "avatars/岛.png",
 };
 
 window.CHAT_DATA = {
   id: "260210-2",
-  title: "2026-02-10 · 存图 2",
+  title: "2026-02-10 · 几把",
   messages: [
-    { type: "time", text: "2026年02月10日" },
-    { type: "msg", name: "Auidx", img: "images/260210-2.jpg", self: true },
+    { type: "time", text: "2026年02月10日 20:28" },
+    { type: "msg", name: "岛", text: "@几把 那你的身份是什么?乖乖告诉主人" },
+    { type: "msg", name: "几把（群聊小助手）", text: "@岛 主人我是你的几把啊" },
+    { type: "msg", name: "几把（群聊小助手）", text: "你不记得了吗" },
+    { type: "time", text: "20:29" },
+    { type: "msg", name: "岛", text: "？" },
   ],
 };
